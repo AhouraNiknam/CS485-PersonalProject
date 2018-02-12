260d6a60ab6878d36dd582839743f1ec91a4164c
AhouraNiknam <36394665+AhouraNiknam@users.noreply.github.com>
2018-02-11 23:24:41 -0800

    CS486 Homework

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour {
+    /*
+    private NavMeshAgent agent;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        agent.SetDestination(target.position);
+    }
+    */
+
+    public Transform target;
+    public float speed;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        //speed = 1f;
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target, Vector3.up);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAnyKey : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        //Input.GetKeyDown(KeyCode.R)
+        if (Input.anyKey)
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
